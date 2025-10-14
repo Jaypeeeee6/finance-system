13e910ec329032e5bda16b753cc5ecdede1071f3
JP Bartolome <jp.bartolome@example.com>
2025-10-14 12:36:20 +0400

    change port

--- app.py
+++ app.py
@@ -1502,5 +1502,5 @@ def internal_error(error):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, host='0.0.0.0', port=5001)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5005)
 
